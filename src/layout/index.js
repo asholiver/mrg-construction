05c122956cecdec945c0935e1e4081cdd341e5f0
Ashley Oliver <ashleyoliver@192-168-1-101.tpgi.com.au>
2019-01-20 22:25:18 +1100

    minor main css tweak

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,5 +1,7 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import Main from "./Main";
+import Page from "./Page";
+import Section from "./Section";
 
-export { Footer, Header, Main };
+export { Footer, Header, Main, Page, Section };
