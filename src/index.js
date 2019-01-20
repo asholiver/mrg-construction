24d1121a1f8d1129ae0b5c9ec3881f16f3690c9a
Ashley Oliver <ashleyoliver@192-168-1-101.tpgi.com.au>
2019-01-20 20:56:00 +1100

    initial css set up

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Icons } from "./components";
+import "./css/Resets.css";
+import "./css/Settings.css";
+import "./css/Helpers.css";
 
 ReactDOM.render(
   <Fragment>
