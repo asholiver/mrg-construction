4652f795db14a2d29fe30e8bff2427d062f7de1d
Ashley Oliver <ashleyoliver@192-168-1-101.tpgi.com.au>
2019-01-22 08:04:20 +1100

    window resize function

--- src/layout/Page/Page.js
+++ src/layout/Page/Page.js
@@ -1,12 +1,26 @@
 import React, { Fragment, Component } from "react";
 import "./Page.css";
+import settings from "./../../css/Settings.css";
 import { Header, Main, Footer } from "..";
 import { Menu, MenuButton } from "./../../components";
 
 class Page extends Component {
+  updateDimensions = this.updateDimensions.bind(this);
   state = {
     activeMenu: false
   };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  updateDimensions() {
+    const pxToEm = window.innerWidth / 16;
+    if (pxToEm > settings.breakPoint) {
+      console.log("updating");
+      this.setState({ activeMenu: false });
+    }
+  }
 
   toggleMenu = e => {
     this.setState({ activeMenu: !this.state.activeMenu });
