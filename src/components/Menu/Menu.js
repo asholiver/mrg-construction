c85bcd8395ff0aa4d43984d0ec3772a28807ef8d
Ashley Oliver <ashleyoliver@192-168-1-101.tpgi.com.au>
2019-01-22 08:04:57 +1100

    menu component WIP

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Menu.css";
+import classNames from "classnames";
+
+const MenuButton = ({ isActive }) => {
+  const classes = classNames({
+    "c-menu-container": true,
+    "is-active": isActive
+  });
+  return (
+    <nav className={classes}>
+      <ul className="c-menu">
+        <li className="c-menu-item ">
+          <NavLink
+            className="c-menu-item__link"
+            activeClassName="is-active"
+            to="/"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="c-menu-item ">
+          <NavLink
+            className="c-menu-item__link"
+            activeClassName="is-active"
+            to="/about_us"
+          >
+            About us
+          </NavLink>
+        </li>
+        <li className="c-menu-item ">
+          <NavLink
+            className="c-menu-item__link"
+            activeClassName="is-active"
+            to="/contact"
+          >
+            Contact
+          </NavLink>
+        </li>
+        <li className="c-menu-item ">
+          <NavLink
+            className="c-menu-item__link"
+            activeClassName="is-active"
+            to="/gallery"
+          >
+            Gallery
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default MenuButton;
