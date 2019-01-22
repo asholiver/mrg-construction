97c049824e38919924525ffb61e0f09284d07b60
Ashley Oliver <ashleyoliver@192-168-1-101.tpgi.com.au>
2019-01-22 11:17:27 +1100

    styling

--- src/layout/Section/Section.js
+++ src/layout/Section/Section.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Section.css";
+import classNames from "classnames";
 
-const Section = ({ children }) => {
-  return <section className="l-section">{children}</section>;
+const Section = ({ image, children }) => {
+  const classes = classNames({
+    "c-section": true,
+    [`c-section--${image}`]: image == null ? false : true
+  });
+  return (
+    <section className={classes}>
+      {image != null ? <div className="c-section--overlay" /> : null}
+      {children}
+    </section>
+  );
 };
 
 export default Section;
