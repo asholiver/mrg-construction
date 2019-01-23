09414b90069833ee04520f92f106019f79a9f858
Ashley Oliver <ashleyoliver@192-168-1-101.tpgi.com.au>
2019-01-23 19:39:17 +1100

    section tweaks

--- src/layout/Section/Section.js
+++ src/layout/Section/Section.js
@@ -2,15 +2,16 @@ import React from "react";
 import "./Section.css";
 import classNames from "classnames";
 
-const Section = ({ image, size, children }) => {
+const Section = ({ image, hasShadow, size, color, children }) => {
   const classes = classNames({
     "c-section": true,
     [`c-section--${image}`]: image == null ? false : true,
-    [`c-section--${size}`]: size == null ? false : true
+    [`c-section--${size}`]: size == null ? false : true,
+    [`c-section--${color}`]: color == null ? false : true
   });
   return (
     <section className={classes}>
-      {image != null ? <div className="c-section--overlay" /> : null}
+      {hasShadow ? <div className="c-section--overlay" /> : null}
       {children}
     </section>
   );
