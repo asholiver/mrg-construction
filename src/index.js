593ea0c78a3d359972002c64e523fb9fbe4c8fd3
Ashley Oliver <ashleyoliver@192-168-1-101.tpgi.com.au>
2019-01-23 19:39:44 +1100

    shame css

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { BrowserRouter, Route } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 import { Icons } from "./components";
 import { Home } from "./containers";
-import "./css/Resets.css";
 import "./css/Settings.css";
+import "./css/Resets.css";
+import "./css/Shame.css";
 import "./css/Helpers.css";
 
 ReactDOM.render(
