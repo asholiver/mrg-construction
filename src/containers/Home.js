a43356a256c78dfc6332b81278c84c2f20d3c573
Ash Oliver <ashleyioliver.1987@gmail.com>
2019-11-05 15:21:15 +0000

    minor tweaks

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -90,41 +90,44 @@ const Home = () => {
         </SectionContent>
       </Section>
       <Section color="grey">
-        <div className="l-grid">
-          <div className="l-grid__item l-grid__item--12-col l-grid__item--6-col-from-lap">
-            <img
-              className="c-testimonial__image"
-              alt="bathroom"
-              src={services}
-            />
-          </div>
-          <div className="l-grid__item l-grid__item--12-col l-grid__item--6-col-from-lap">
-            <div className="c-testimonial">
-              <dl className="c-testimonial__list">
-                <div>
-                  <dt className="c-testimonial__title">Shelley Christie</dt>
-                  <dd className="c-testimonial__text">
-                    Fantastic work....very well priced and top professional
-                    results.
-                  </dd>
-                </div>
-                <div>
-                  <dt className="c-testimonial__title">Martyn Sign</dt>
-                  <dd className="c-testimonial__text">
-                    Great work. Honest and reliable. Couldn’t ask for more.
-                    Highly recommended!
-                  </dd>
-                </div>
-                <div>
-                  <dt className="c-testimonial__title">Andy Christie</dt>
-                  <dd className="c-testimonial__text">
-                    Loads of experience... gives good advice
-                  </dd>
-                </div>
-              </dl>
+        <SectionHeader isUnderlined={true} title="Testimonials" />
+        <SectionContent>
+          <div className="l-grid">
+            <div className="l-grid__item l-grid__item--12-col l-grid__item--6-col-from-lap">
+              <img
+                className="c-testimonial__image"
+                alt="bathroom"
+                src={services}
+              />
+            </div>
+            <div className="l-grid__item l-grid__item--12-col l-grid__item--6-col-from-lap">
+              <div className="c-testimonial">
+                <dl className="c-testimonial__list">
+                  <div>
+                    <dt className="c-testimonial__title">Shelley Christie</dt>
+                    <dd className="c-testimonial__text">
+                      Fantastic work....very well priced and top professional
+                      results.
+                    </dd>
+                  </div>
+                  <div>
+                    <dt className="c-testimonial__title">Martyn Sign</dt>
+                    <dd className="c-testimonial__text">
+                      Great work. Honest and reliable. Couldn’t ask for more.
+                      Highly recommended!
+                    </dd>
+                  </div>
+                  <div>
+                    <dt className="c-testimonial__title">Andy Christie</dt>
+                    <dd className="c-testimonial__text">
+                      Loads of experience... gives good advice
+                    </dd>
+                  </div>
+                </dl>
+              </div>
             </div>
           </div>
-        </div>
+        </SectionContent>
       </Section>
     </Page>
   );
